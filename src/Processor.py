49759db9c7568c6464527960f5cf52361d66fc45
C. H. Lay <development@cschlay.com>
2018-12-23 14:03:37 +0200

    Changed accumulator into general register.

--- src/Processor.py
+++ src/Processor.py
@@ -6,20 +6,33 @@ from Register import Register
 
 
 class Processor:
+    """
+    Emulates a processor for RASP-Machine.
+    https://en.wikipedia.org/wiki/Random-access_stored-program_machine
+    """
+    REGISTER_COUNT = 8
+
     def __init__(self):
-        self.accumulator: BinaryNumber
-        self.memory: list = [Register] * 8
+        """
+        Constructs a new virtual processor with pre-defined register count.
+        Register 0 is used as accumulator.
+        Register 1 is used as instruction counter.
+        The rest are freely usable.
+        """
+        self.registers: list = [Register] * Processor.REGISTER_COUNT
         self.instruction_memory: list = []
 
     def load(self, immediate: BinaryNumber):
-        self.accumulator = immediate
+        """
+        Instruction: LDA, I
+        Load a value to the accumulator.
+        """
+        self.registers[0] = immediate
 
     def add(self, immediate: BinaryNumber):
         """
-        Could be thought as accumulator = accumulator + immediate.
-
-        :param immediate:
-        :return:
+        Instruction: ADD, I
+        Performs addition to value in the accumulator such that accumulator += I.
         """
         A: BinaryNumber = self.accumulator
         B: BinaryNumber = immediate
@@ -36,7 +49,27 @@ class Processor:
                 C[i] = A[i] + B[i] + carry
                 carry = 0
 
-        self.accumulator = C
+        self.registers[0] = C
 
         if carry == 1:
             print("OVERFLOW")
+
+    def branch(self, address: int):
+        """
+        Instruction: BPA, A
+        Sets the instruction counter to value A if the accumulator is greater than 0.
+        """
+        if self.registers[0].decimal() > 0:
+            self.registers[1] = address     # Set the instruction counter to address A.
+
+    def read(self):
+        pass
+
+    def store(self):
+        pass
+
+    def substract(self):
+        pass
+
+    def print(self, address: int):
+        print(self.registers[address])
