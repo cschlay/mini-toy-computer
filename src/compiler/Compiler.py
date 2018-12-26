8087d781f3c9711c2beae2e5ed0deb52eb7525ce
C. H. Lay <development@cschlay.com>
2018-12-26 14:08:31 +0200

    Compiler should now work as expected.

--- src/compiler/Compiler.py
+++ src/compiler/Compiler.py
@@ -2,9 +2,12 @@ import os
 
 
 class Compiler:
-    INSTRUCTION_SET = {
-        'ADD', 'SUB', 'MUL',
-    }
+    """
+    Makes a program executable.
+    """
+    INSTRUCTION_SET = {"ADD", "BPA", "CPY", "HALT", "LDA", "PRINT", "READ", "SUB"}
+    # The instructions that are build from elementary instructions and stored in compiler/extended_instructions.
+    EXTENDED_INSTRUCTIONS = {}
 
     def __init__(self, root_directory: str = "programs"):
         self.ROOT: str = root_directory
@@ -13,25 +16,45 @@ class Compiler:
             os.makedirs(self.COMPILED)
 
     def compile(self, file: str):
+        """
+        Compiles a program such that it can be run.
+        """
         try:
-            with open(file) as program:
-                for line in program:
-                    pass
-        except:
-            print("ERROR: Program not found, did you compile it first?")
+            program = open(f"{self.ROOT}/{file}", 'r')
 
-def process_line(self, line: str) -> [str] or None:
+            executable = None
+            try:
+                executable = open(f"{self.COMPILED}/{file}", 'x')
+            except FileExistsError:
+                executable = open(f"{self.COMPILED}/{file}", 'w')
+
+            # Process the lines and write them if possible.
+            for line in program:
+                processed_line: str = process_line(line)
+                if processed_line is not None:
+                    executable.write(processed_line)
+
+            program.close()
+            executable.close()
+        except FileNotFoundError:
+            print("ERROR: Program not found, most likely in wrong directory. Should be at programs/.")
+
+
+def process_line(line: str) -> [str] or None:
     """
     Pre-processes a line by removing comments and whitespaces.
     Return a line as an array.
     If a line does not have valid instructions, it will return None.
     """
-    line_splitted: list = line.split()
-    if line_splitted[0] in Compiler.INSTRUCTION_SET:
-        cut_index = 0
-        for token in range(len(line_splitted)):
-            if token == '#':
-                cut_index = token
-        return line_splitted[:cut_index]
+    # Remove comments
+    if '#' in line:
+        cut = line.find('#')
+        line = line[:cut]
+
+    # Formatting the string.
+    line = line.replace('\n', '').replace('_', '').strip()
+
+    if len(line) >= 1 and line.split()[0] in Compiler.INSTRUCTION_SET or Compiler.EXTENDED_INSTRUCTIONS:
+        return line
     else:
         return None
---
+++ src/programs/demo.asm
@@ -0,0 +1,3 @@
+# This is a demo program.
+
+LDA 0000_1000 # Load value 8 to accumulator.
